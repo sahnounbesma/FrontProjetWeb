a8eebe287b9a853871c61f3d888981ecd2e996d4
Besma <fb_sahnoun@esi.dz>
2021-01-14 23:25:58 +0100

    ok

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+//app.use(express.static('C:/Users/sahno/Desktop/projetWebFront/src/app/components'));
+
+//Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('C:/Users/sahno/Desktop/projetWebFront/src/app/components'));
-//app.use(express.static('./dist/projetweb'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/projetweb'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('components.component.html', {root: 'C:/Users/sahno/Desktop/projetWebFront/src/app/components'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/projetweb/index.html'));
+});
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
